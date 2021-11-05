a681f2ed170865132e823df1c5186bc2d0a9a083
ilyak <ilyakozorezov@gmail.com>
2021-11-04 21:38:53 -0700

    initial commit

---
+++ Maze.java
@@ -0,0 +1,9 @@
+public class Maze {
+    Question testQ = new Question("test" , new String[]{"test"},0);
+    public Maze(){
+
+    }
+    private void createMaze(){
+        
+    }
+}
---
+++ Question.java
@@ -0,0 +1,23 @@
+public final class Question {
+    private final String myQuestion;
+    private final String[] myOptions;
+    private final int myAnswer;
+
+    public Question(final String theQuestion , final String[] theOptions, final int theAnswer){
+        myQuestion = theQuestion;
+        myOptions = theOptions;
+        myAnswer = theAnswer;
+    }
+
+    public String getMyQuestion() {
+        return myQuestion;
+    }
+
+    public String[] getMyOptions() {
+        return myOptions;
+    }
+
+    public int getMyAnswer() {
+        return myAnswer;
+    }
+}
