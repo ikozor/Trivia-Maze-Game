a1091ce51dd05a124ea189439ef89231beca5d72
ikozor <ilyakozorezov@gmail.com>
2021-11-08 12:58:39 -0800

    Added Graph

---
+++ Graph.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+
+public class Graph {
+    private HashMap<Integer , HashSet<Integer>> myGraph;
+
+    public Graph(){
+        myGraph = new HashMap<>();
+    }
+
+    public void addVertex(final int theNumber){
+        myGraph.put(theNumber, new HashSet<>());
+    }
+
+    public void addEdge(final int theStart, final int theEnd){
+        if(!myGraph.containsKey(theStart))
+            myGraph.put(theStart, new HashSet<>());
+
+        if(!myGraph.containsKey(theEnd))
+            myGraph.put(theEnd, new HashSet<>());
+
+        myGraph.get(theStart).add(theEnd);
+        myGraph.get(theEnd).add(theStart);
+    }
+
+    public void removeEdge(final int theStart, final int theEnd){
+        if(!myGraph.containsKey(theStart)) {
+            System.out.println("Graph:\nTHE (start to) EDGE TO BE DELETED DOES NOT EXIST");
+            return;
+        }
+
+        if(!myGraph.containsKey(theEnd)) {
+            System.out.println("Graph:\nTHE (end to) EDGE TO BE DELETED DOES NOT EXIST");
+            return;
+        }
+
+        myGraph.get(theStart).remove(theEnd);
+        myGraph.get(theEnd).remove(theStart);
+    }
+
+    public boolean hasEdge(final int theStart, final int theEnd){
+        return myGraph.containsKey(theStart) && myGraph.get(theStart).contains(theEnd);
+    }
+
+}
