d46169d5cdbb96640b89278fc28a9b82bd4f5601
ikozor <ilyakozorezov@gmail.com>
2021-11-09 18:27:45 -0800

    main

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    Maze test = new Maze(36);
-        System.out.println(test.toString());
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Maze test = new Maze(9);
+        System.out.println(test.closeDoor(0, 1));
+        System.out.println(test.closeDoor(0, 3));
+        System.out.println(test);
+
     }
 }
