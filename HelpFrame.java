75309fed3530ee3b6d1fd87535f67e94acb32e30
ilyak <ilyakozorezov@gmail.com>
2021-12-14 17:59:55 -0800

    "Added help page"

--- HelpFrame.java
+++ HelpFrame.java
@@ -16,26 +16,27 @@ public class HelpFrame extends JFrame {
 
     private JPanel helpPanel(){
         JPanel panel = new JPanel();
-        String text = "Welcome to Python Trivia Maze!\n" +
-                "Here you can test your python skills by answering questions.\n" +
-                "If you are on a streak, you will be able to do a python challenge to increase your score!\n" +
-                "Your goal is to go through the maze and try to make it to the bottom right corner\n" +
-                "\n" +
-                "Select:\n" +
-                "New Game to start a new game\n" +
-                "Load Game to continue playing from the last saved game\n" +
-                "\n" +
-                "You will start in the top right corner and be able to move in directions RIGHT, LEFT, UP, and DOWN\n" +
-                "After selecting a direction, you will be asked a question and options\n" +
-                "If you answer correctly, the player will move to that new position and the room will be unlocked\n" +
-                "If you answer incorrectly, the door to the room will be locked\n" +
-                "\n" +
-                "Press Save to save the game\n" +
-                "Press Exit to quit the game.\n" +
-                "\n" +
-                "In Settings\n" +
-                "Select Mute to mute audio,\n" +
-                "Select Stack Overflow to enable cheats";
+        String text = """
+                Welcome to Python Trivia Maze!
+                Here you can test your python skills by answering questions.
+                If you are on a streak, you will be able to do a python challenge to increase your score!
+                Your goal is to go through the maze and try to make it to the bottom right corner
+
+                Select:
+                New Game to start a new game
+                Load Game to continue playing from the last saved game
+
+                You will start in the top right corner and be able to move in directions RIGHT, LEFT, UP, and DOWN
+                After selecting a direction, you will be asked a question and options
+                If you answer correctly, the player will move to that new position and the room will be unlocked
+                If you answer incorrectly, the door to the room will be locked
+
+                Press Save to save the game
+                Press Exit to quit the game.
+
+                In Settings
+                Select Mute to mute audio,
+                Select Stack Overflow to enable cheats""";
 
         JTextArea textArea = new JTextArea(text);
         textArea.setFont(new Font(Font.DIALOG, Font.BOLD, 15));
